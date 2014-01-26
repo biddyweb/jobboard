abe75254c23fb836f8110347d0d3fa384b2775ca
RobertZK <technoguyrob@gmail.com>
2014-01-25 20:06:32 -0600

    obviate the need to specify sources explicitly

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,20 @@
-module Scraper
+# So that Scraper.sources below works correctly (note Rails' autoloading)
+require_all "#{Rails.root}/lib/scraper"
 
-  SOURCES = ["Eightythousand"]
+module Scraper
 
   def self.run source
     Engine.new(source).scrape
   end
 
   def self.run_all
-    SOURCES.map { |source| { source => run(source) } }.reduce(:merge)
+    sources.map { |source| { source => run(source) } }.reduce(:merge)
+  end
+
+  def self.sources
+    # http://stackoverflow.com/questions/833125/find-classes-available-in-a-module
+    Scraper.constants.select { |c| Class === Scraper.const_get(c) }
+      .select { |klass| "Scraper::#{klass}".constantize < Scraper::Processor }
   end
 
 end
