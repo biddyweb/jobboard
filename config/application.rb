146a53209a7ed5fefa8909c076d459101925846e
RobertZK <technoguyrob@gmail.com>
2014-01-25 20:10:28 -0600

    fix spacing

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,10 @@ require "sprockets/railtie"
 Bundler.require(:default, Rails.env)
 
 module Eajobsboard
-	class Application < Rails::Application
+  class Application < Rails::Application
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.autoload_paths += %W(#{config.root}/lib)
 
-	end
+  end
 end
