538e9af429f9c6fa6cbfae090625b1d79c4b6ad4
Peter Hurford <peter@peterhurford.com>
2014-02-19 19:15:46 -0500

    Messed with scraper

--- lib/scraper/humaneleaguejobs.rb
+++ lib/scraper/humaneleaguejobs.rb
@@ -14,33 +14,7 @@ module Scraper
     def scrape agent
       jobs = []
       agent.get(SOURCE) do |page|
-
-        jobs_div = page / 'BOC'
-        buffer = []
-        print jobs_div[1]
-        for child in jobs_div[1].children
-          if child.name == 'strong class="style16"'
-            jobs << create_job_from_xml!(buffer)
-            buffer = []
-          end
-          buffer << child
-        end
-      end
-      jobs.compact
-    end
-
-    def create_job_from_xml! xml
-      unless Job.where('title like ?', "%#{xml[0].text}%")
-                .where(org: ORGANIZATION).exists? # TODO: Move to Engine
-        Job.new \
-          title: xml[0].text,
-          org: ORGANIZATION,
-          internship: False,
-          postdate: nil, filldate: nil, # Doesn't appear to be shown...
-          location: LOCATION,
-          description: xml[1..-1].map(&:to_html).join,
-          link: SOURCE,
-          user_id: 1
+        print page
       end
     end
   end
