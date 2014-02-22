2006e8fd6662e3c79c6cd8e87fa2b63f662d3037
Peter Hurford <peter@peterhurford.com>
2014-02-22 13:15:56 -0500

    IPA Scraper Draft 2

--- lib/scraper/ipa.rb
+++ lib/scraper/ipa.rb
@@ -17,56 +17,22 @@ module Scraper
         links = page / "div#content" / "a"
         links = links.to_a
         links = links.select do |link|
-          link.attributes["href"].to_s =~ /jobs\//
-          # Noticed that only legitimate job links have a URL 
-          # descriptor with a path after "jobs".
+          link.attributes["href"].to_s =~ /jobs\// # Noticed that only legitimate job links have a URL descriptor with a path after "jobs".
         end
-        puts links.map(&:to_html)
-
-        #  $link.methods - Class.new.methods
-
-=begin
-
-        jobs_div = page / "div.col-md-9"
-        buffer = []
-
-        for child in jobs_div[1].children
-          if child.name == "h2"
-            jobs << create_job_from_xml!(buffer)
-            buffer = []
-          end
-          buffer << child
+        links.each do
+          scrape_job(agent, links)
         end
-        jobs.shift if jobs.length > 0 # Throw away first entry
-=end
-      end
-      jobs.compact
-    end
-
-    def create_job_from_xml! xml
-      unless Job.where('title like ?', "%#{xml[0].text}%")
-                .where(org: ORGANIZATION).exists? # TODO: Move to Engine
-        Job.new \
-          title: xml[0].text,
-          org: ORGANIZATION,
-          internship: !!(xml[0].text =~ /intern/i),
-          postdate: nil, filldate: nil, # Doesn't appear to be shown...
-          location: LOCATION,
-          description: xml[1..-1].map(&:to_html).join,
-          link: extract_link_from_xml(xml),
-          user_id: 1
       end
     end
 
-    def extract_link_from_xml xml
-      # A little tricky. Loop for <a> elements and extract href attribute
-      # of first one containing text 'this form'.
-      link = xml.map { |y| y / 'a' }.flatten
-        .select { |y| y.text =~ /this form/i }.first
-        .try(:attributes).try(:[], "href").try(:value)
-      link || SOURCE
+    def scrape_job agent, links
+      agent.get(|links|) do |page|
+        job = page / "div#content"
+        puts job
+        puts "---"
+      end
     end
-
+  
   end
 
 end
