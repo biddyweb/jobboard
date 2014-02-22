5a8bef6e20c0be31150b7523c63c94f03580585f
Peter Hurford <peter@peterhurford.com>
2014-02-22 13:24:23 -0500

    IPA, Draft 5

--- lib/scraper/ipa.rb
+++ lib/scraper/ipa.rb
@@ -19,15 +19,13 @@ module Scraper
         links = links.select do |link|
           link.attributes["href"].to_s =~ /jobs\// # Noticed that only legitimate job links have a URL descriptor with a path after "jobs".
         end
-        links.each do
-          links.map! { |link| link.attributes["href"].to_s }
-          scrape_job(agent, links)
-        end
+        links.map do { |link| link.attributes["href"].to_s }
+        links.each do { |link| scrape_job(agent, link) }
       end
     end
 
-    def scrape_job agent, links
-      agent.get(links) do |page|
+    def scrape_job agent, link
+      agent.get(link) do |page|
         job = page / "div#content"
         puts job
         puts "---"
