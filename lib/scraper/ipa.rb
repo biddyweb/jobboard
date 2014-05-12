28a5b2885885f894369ac4b2a6024d07e871d5ba
Peter Hurford <peter@peterhurford.com>
2014-05-12 16:48:02 -0400

    Finished styling IPA jobs

--- lib/scraper/ipa.rb
+++ lib/scraper/ipa.rb
@@ -42,8 +42,19 @@ module Scraper
       
       content_t = content.to_html                                 # Change content from Nokogiri object to HTML
       content_t.gsub!(/\<h1(.+)\<\/h1\>/m, '')                    # Remove title from description
-      content_t.gsub!(/\<ul class\=\"links(.+)\<\/div\>/m, '')    # Remove "Print this page" and "Share" links from description (this isn't working and I don't know why)
+      content_t.gsub!(/\<ul class\=\"links(.+)\<\/div\>/m, '')    # Remove "Print this page" and "Share" links from description
 
+      # Remove annoying styling
+      p1style = "color: rgb(57, 57, 57); line-height: 1.5em; margin: 0px 0px 1em; font-family: Helvetica, Arial, 'Nimbus Sans L', sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);"
+      p2style = "color: rgb(57, 57, 57); line-height: 18pt; margin: 0px 0px 12pt; font-family: Helvetica, Arial, 'Nimbus Sans L', sans-serif; font-size: 14px; background-color: white;"
+      ulstyle = "color: rgb(57, 57, 57); line-height: 1.5em; margin: 1em 0px; padding-left: 2em; font-family: Helvetica, Arial, 'Nimbus Sans L', sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);"
+      span1style = "font-size: 10.5pt; font-family: Helvetica, sans-serif;"
+      span2style = "color: rgb(57, 57, 57); font-family: Helvetica, Arial, 'Nimbus Sans L', sans-serif; font-size: 14px; line-height: 20.400001525878906px; background-color: rgb(255, 255, 255);"
+      content_t.gsub!('<p style="' + p1style + '">', "<p>")
+      content_t.gsub!('<p style="' + p2style + '">', "<p>")
+      content_t.gsub!('<ul style="' + ulstyle + '">', "<ul>")
+      content_t.gsub!('<span style="' + span1style + '">', "<span>")
+      content_t.gsub!('<span style="' + span2style + '">', "<span>")
 
       # Grab information from ul elements on page
       list_els = content / "li"
