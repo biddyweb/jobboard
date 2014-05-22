6180a361c7c462ef57ae48ba3310efad6d7d31b7
Peter Hurford <peter@peterhurford.com>
2014-05-22 11:40:23 -0400

    Added PSI scraper (finished, but with fatal bug)

--- lib/scraper/psi.rb
+++ lib/scraper/psi.rb
@@ -1,5 +1,5 @@
 module Scraper 
-  # use mechanize to scrape from https://www.poverty-action.org/getinvolved/jobs
+  # use mechanize to scrape from PSI
   # mechanize: http://mechanize.rubyforge.org/Mechanize.html
 	
   class PSI < Processor
@@ -13,11 +13,11 @@ module Scraper
 
     def scrape agent    # Scrape page for job pages, and then create a job out of each link
       jobs = nil
-      agent.get(SOURCE) do |page|                                 # Begin page scraping using Mechanize
-        links = page / "div.jvlisting" / "a"                      # Use XPATH to get all links within the jvlisting div wrapper from the page
-        links = links.to_a                                        # Turn list of links into an array
-        4.times { links.shift }                                   # Throw out first four links (not job related)
-        jobs = links.map do |link|                                # Scrape a job from each link
+      agent.get(SOURCE) do |page|                                               # Begin page scraping using Mechanize
+        links = page / "div.jvlisting" / "a"                                    # Use XPATH to get all links within the jvlisting div wrapper from the page
+        links = links.to_a                                                      # Turn list of links into an array
+        4.times { links.shift }                                                 # Throw out first four links (not job related)
+        jobs = links.map do |link|                                              # Scrape a job from each link
           next if Job.where(title: link.text).where(org: ORGANIZATION).exists?
           middle_man(agent, link.attributes["href"].to_s)
         end
@@ -25,47 +25,47 @@ module Scraper
       jobs.compact
     end
 
-    def middle_man agent, link                                    # PSI uses an annoying frame, so we need to get the frame and then find the real job content there
+    # PSI uses a really annoying frame, so we need to get the frame and then find the real job content there
+    def middle_man agent, link
       agent.get(link) do |page|
-        content = page / "frame#jobviteframe"                     # Grab frame (not sure this works)
-        raise content.inspect
+        url = page / "div#node-5647"                                            # Grab frame, which is wrapped in the node-5647 div tag
+        url = url.children[3]['src']                                            # Grab URL out of frame
+        flag = link.gsub!('http://www.psi.org/jobs/all-positions?jvi=', '')     # We need to get the job id out of the URL and pass it along in order to get the frame right.
+        url = url + '&j=' + flag                                                # Append the flag to the URL 
+        job = scrape_job(agent, url, link)                                      # Get the job from the frame URL.  Also pass original link so we can keep it for records.
       end
-      scrape_job(agent, link)
     end
 
-    def scrape_job agent, link                                    # Prep each individual job page
+    def scrape_job agent, link, original                                        # Prep each individual job page
       job = nil
-      agent.get(link) do |page|                                   # Scrape page using Mechanize
-        content = page / "div.jobDisplay"                         # Get job content from the div jobDisplay wrapper
-        job = create_job_from_content! content, link              # Create a job out of it
+      agent.get(link) do |page|                                                 # Scrape page using Mechanize
+        content = page / "div.jvdescription"                                    # Get job content from the div jvdescription wrapper
+        job = create_job_from_content! content, link, original                  # Create a job out of it
       end
       job
     end
 
-    def create_job_from_content! content, link                    # Actually put the job in the database
-      title = content / "div.jobTitle" / "h1#job-title"           # Grab title object from the job-title h1 tag within the jobTitle div tag
-      title = title.children[0].text                              # Grab title text from the title object
-      title = title.gsub(' Job', '')                              # Remove "Job" from title
+    def create_job_from_content! content, link, original                        # Actually put the job in the database
+      title = content / "div.jvjobheader" / "h2"                                # Grab title object from the h2 tag within the jvjobheader div tag
+      title = title.text                                                        # Grab title text from the title object
 
-      location = content / "p#job-location"                       # Grab location object from the job-location p tag
-      location = location.children[3].children[0].text            # Grab location text from the location object
-
-      startdate = content / "p#job-date"                          # Grab start date object from the job-date p tag
-      startdate = startdate.children[3].children[0].text          # Grab location text from the location object
+      location = content / "div.jvjobheader" / "h3"                             # Grab location object from the h3 tag within the jvjobheader div tag
+      location = location.text                                                  # Grab location text from the location object
+      location = location.gsub('Affiliate/Platform-Based | ', '')               # Remove extraneous text from location
       
-      content_t = content / "div.job"                             # Split out all the unnecessary wrapping
-      content_t = content_t.to_html                               # Change content from Nokogiri object to HTML
+      content_t = content / "div.jvdescriptionbody"                             # Split out all the unnecessary wrapping
+      content_t = content_t.to_html                                             # Change content from Nokogiri object to HTML
       
       # A check for existence is already present in the scrape method
       Job.new \
         title: title,
         org: ORGANIZATION,
-        internship: false,
-        postdate: startdate,
+        internship: !!(title =~ /intern/i),
+        postdate: nil,
         filldate: nil,
         location: location,
         description: content_t,
-        link: link,
+        link: original,
         user_id: 1
     end
 
